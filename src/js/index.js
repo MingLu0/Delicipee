5d179477714f688e38263cf97d5be9d0618c20fa
Ming Luo <luoming886@gmail.com>
2019-10-21 21:05:55 +0800

    added babel

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 // Global app controller
 import num from './test';
 
-console.log(`I imported ${num}from another module`);
+const x = 45;
+
+console.log(`I imported ${num}from another module and the x value is ${x}`);
