86e69beb50bcd8e670ef57992049add98585c072
Ming Luo <luoming886@gmail.com>
2019-10-24 21:43:33 +0800

    pulled data recipes data from api

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,21 @@
 // Global app controllor
 
 import {elements} from './views/base';
+import Search from './models/SearchRecipe';
 
+const state = {};
+const searchRecipes = async () =>{
+    const query = 'pizza';
+    if(query){
+        state.search = new Search(query);
 
+        await state.search.getRecipes();
+
+        console.log(state.search.result)
+    }
+}
 elements.searchSubmit.addEventListener('submit',e=>{
 e.preventDefault();
-
-console.log(elements.searchInput.value);
+searchRecipes();
+console.log('hello world');
 });
---
+++ src/js/models/SearchRecipe.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export default class SearchRecipe{
+
+    constructor(query){
+        this.query = query;
+    }
+
+    async getRecipes(){
+        const proxy = 'https://cors-anywhere.herokuapp.com/';
+        const key = '4d78d05d9f20215c272d04f6974c04db';
+        try{
+            const res = await axios(`https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+        }catch(error){
+            alert(error);
+        }
+    }
+    
+}
